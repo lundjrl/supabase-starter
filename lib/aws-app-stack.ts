f39644c4bc68751a24dc1566c2ab51f0b135e1a5
lundjrl <jrlbidamin@gmail.com>
2023-08-09 20:34:11 -0400

    Got AWS CDK to work

--- lib/aws-app-stack.ts
+++ lib/aws-app-stack.ts
@@ -1,15 +1,20 @@
-import * as apprunner_alpha from '@aws-cdk/aws-apprunner-alpha';
-import * as cdk from 'aws-cdk-lib';
-import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets';
-import { Construct } from 'constructs';
+import * as apprunner_alpha from '@aws-cdk/aws-apprunner-alpha'
+import * as cdk from 'aws-cdk-lib'
+import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets'
+import { Construct } from 'constructs'
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 
 export class AwsAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+    })
 
     const imageAsset = new ecrAssets.DockerImageAsset(this, 'Image', {
       directory: './webapp',
-    });
+    })
 
     const service = new apprunner_alpha.Service(this, 'NextJsService', {
       source: apprunner_alpha.Source.fromAsset({
@@ -17,12 +22,9 @@ export class AwsAppStack extends cdk.Stack {
       }),
       cpu: apprunner_alpha.Cpu.ONE_VCPU,
       memory: apprunner_alpha.Memory.TWO_GB,
-    });
-    service.addEnvironmentVariable(
-      'RANDOM_IMAGE_API_URL',
-      'https://random.imagecdn.app/500/500'
-    );
+    })
+    service.addEnvironmentVariable('RANDOM_IMAGE_API_URL', 'https://random.imagecdn.app/500/500')
 
-    new cdk.CfnOutput(this, 'ServiceUrl', { value: service.serviceUrl });
+    new cdk.CfnOutput(this, 'ServiceUrl', { value: service.serviceUrl })
   }
 }
