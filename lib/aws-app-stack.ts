668df447ec5d32fb47fdca5908c879ad1294b6d6
lundjrl <jrlbidamin@gmail.com>
2023-08-12 23:19:31 -0400

    Saving work

--- lib/aws-app-stack.ts
+++ lib/aws-app-stack.ts
@@ -1,8 +1,9 @@
-import * as apprunner_alpha from '@aws-cdk/aws-apprunner-alpha'
+import * as apprunner from 'aws-cdk-lib/aws-apprunner'
 import * as cdk from 'aws-cdk-lib'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets'
-import { Construct } from 'constructs'
 import { aws_s3 as s3 } from 'aws-cdk-lib'
+import { AppRunnerAutoScaling } from 'cdk-apprunner-autoscaling'
 
 export class AwsAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -16,15 +17,56 @@ export class AwsAppStack extends cdk.Stack {
       directory: './webapp',
     })
 
-    const service = new apprunner_alpha.Service(this, 'NextJsService', {
-      source: apprunner_alpha.Source.fromAsset({
-        asset: imageAsset,
-      }),
-      cpu: apprunner_alpha.Cpu.ONE_VCPU,
-      memory: apprunner_alpha.Memory.TWO_GB,
+    // create the Auto Scaling Configuration
+    const autoScalingConfiguration = new AppRunnerAutoScaling(this, 'AutoScalingConfiguration', {
+      AutoScalingConfigurationName: `demo`,
+      MaxConcurrency: 50,
+      MinSize: 1,
+      MaxSize: 1,
     })
-    service.addEnvironmentVariable('RANDOM_IMAGE_API_URL', 'https://random.imagecdn.app/500/500')
 
-    new cdk.CfnOutput(this, 'ServiceUrl', { value: service.serviceUrl })
+    // create the access role to pull the image from ECR
+    const accessRole = new iam.Role(this, 'AccessRole', {
+      assumedBy: new iam.ServicePrincipal('build.apprunner.amazonaws.com'),
+    })
+    accessRole.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ['ecr:GetAuthorizationToken'],
+        resources: ['*'],
+      }),
+    )
+
+    // create the App Runner service
+    const service = new apprunner.CfnService(this, 'WebApp', {
+      autoScalingConfigurationArn: autoScalingConfiguration.arn,
+      instanceConfiguration: {
+        cpu: '1 vCPU',
+        memory: '2 GB',
+      },
+      sourceConfiguration: {
+        authenticationConfiguration: {
+          accessRoleArn: accessRole.roleArn,
+        },
+        autoDeploymentsEnabled: true,
+        imageRepository: {
+          imageRepositoryType: 'ECR',
+          imageIdentifier: imageAsset.imageUri,
+          imageConfiguration: {
+            runtimeEnvironmentVariables: [
+              {
+                name: 'RANDOM_IMAGE_API_URL',
+                value: 'https://random.imagecdn.app/500/500',
+              },
+            ],
+          },
+        },
+      },
+      networkConfiguration: {
+        egressConfiguration: {
+          egressType: 'DEFAULT',
+        },
+      },
+    })
+    new cdk.CfnOutput(this, 'ServiceUrl', { value: service.attrServiceUrl })
   }
 }
